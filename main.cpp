9165a03522e708ba85eb2eae02a7d0e91a344a83
Tiago <tiagoprysthon14@gmail.com>
2023-02-17 17:59:31 -0300

    [refactor] change cout to printf

--- main.cpp
+++ main.cpp
@@ -11,58 +11,59 @@ int main() {
   int registerMonth, selectedMonth;
   int recebidos, atendidos;
   int months[12][2] = {{5, 3}, {4, 3}, {6, 6}, {2, 1}, {6, 3}, {9, 5}, {4, 1}, {7, 4}, {3, 0}, {4, 2}, {6, 2}, {4, 4}};
-  string monthsName[12] = {"janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
+  std::string monthsName[12] = {"janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
   bool loop = true;
   
   while (loop == true) {
 
-    cout << "Registro ou Exibição?" << endl;
+    printf("Registro ou Exibição?\n");
     cin >> type;
-    cout << endl;
+    printf("\n");
     
     if (type == "Registro" || type == "registro") {
-    cout << "Informe um mês (1-12)" << endl;
-    cin >> registerMonth;
-    cout << endl;
-    registerMonth -= 1;
-
-    cout << "Informe  o número de chamados recebidos no mês de " << monthsName[registerMonth] << ":" << endl;
-    cin >> recebidos;
-    cout << endl;
-    cout << "Informe  o número de chamados atendidos no mês de " << monthsName[registerMonth] << ":" << endl;
-    cin >> atendidos;
-    cout << endl;
-
-    months[registerMonth][0] = recebidos;
-    months[registerMonth][1] = atendidos;
+      printf("Informe um mês (1-12)\n");
+      cin >> registerMonth;
+      printf("\n");
+      registerMonth -= 1;
+  
+      printf("Informe  o número de chamados recebidos no mês de %s:\n", monthsName[registerMonth].c_str());
+      cin >> recebidos;
+      printf("\n");
+      printf("Informe  o número de chamados atendidos no mês de %s:\n", monthsName[registerMonth].c_str());
+      cin >> atendidos;
+      printf("\n");
+  
+      months[registerMonth][0] = recebidos;
+      months[registerMonth][1] = atendidos;
 
   } else if (type == "Exibição" || type == "exibição") {
-    cout << "Informe 0 (zero) se quiser ver os  dados referentes a todos os meses, ou um número de 1 a 12, se desejar ver os dados de um mês específico" << endl;
+      printf("Informe 0 (zero) se quiser ver os  dados referentes a todos os meses, ou um número de 1 a 12, se desejar ver os dados de um mês específico:\n");
     cin >> selectedMonth;
-    cout << endl;
+    printf("\n");
     selectedMonth -= 1;
 
     if (selectedMonth == -1) {
       for (int i = 0; i < 12; i += 1) {
-        cout << monthsName[i] << ": " << endl;
-        cout << "Chamados recebidos: " << months[i][0] << endl;
-        cout << "Chamados atendidos: " << months[i][1] << endl;
-        cout << endl;
+        printf("%s\n", monthsName[i].c_str());
+        printf("Chamados recebidos: %d\n", months[i][0]);
+        printf("Chamados atendidos: %d\n", months[i][1]);
+        printf("\n");
       }
 
     } else {
-      cout << "Chamados recebidos no mês de " << monthsName[selectedMonth] << " foi de: " << months[selectedMonth][0] << endl;
-      cout << "Chamados atendidos no mês de " << monthsName[selectedMonth] << " foi de: " << months[selectedMonth][1] << endl;
+      printf("Chamados recebidos no mês de %s foi de: %d\n", monthsName[selectedMonth].c_str(), months[selectedMonth][0]);
+      printf("Chamados atendidos no mês de %s foi de: %d\n", monthsName[selectedMonth].c_str(), months[selectedMonth][1]);
+      printf("\n");
     }
   }
 
-  cout << "deseja continuar? S/N" << endl;
+  printf("Deseja continuar? S/N\n");
   cin >> continuar;
-  cout << endl;
+  printf("\n");
   if (continuar == "N") loop = false;
   }
   
-  cout << "Sua operação foi encerrada. Obrigado!" << endl;
+  printf("Sua operação foi encerrada. Obrigado!\n");
   
   return 0;
 }
